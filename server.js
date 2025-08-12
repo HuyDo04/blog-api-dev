618eeaa2ce118f771a17ead190979d62f09e8f68
HUYDO <huydo041203@gmail.com>
2025-08-13 03:26:40 +0700

    update

--- server.js
+++ server.js
@@ -1,38 +1,34 @@
-require('dotenv').config({ path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env' })
-require('module-alias/register')
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env'
+});
+require('module-alias/register');
+
+console.log('PORT:', process.env.PORT);
+console.log('CLIENT_ORIGIN:', process.env.CLIENT_ORIGIN);
+console.log('NODE_ENV:', process.env.NODE_ENV);
 const cors = require("cors");
-const express = require('express')
-const router = require("./src/routes")
-const handleSession = require("@/middleware/handleSession");
+const express = require('express');
+const router = require("./src/routes");
 const cookieParser = require('cookie-parser');
 
-const app = express()
-app.set('etag', false);
+const app = express();
+
 const port = process.env.PORT || 3000;
-const allowedOrigins = [
-  process.env.FRONTEND_URL || "https://titokhd.online",
-  "http://localhost:5173"
-];
+const clientOrigin = process.env.CLIENT_ORIGIN || "http://localhost:5173";
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
+  origin: clientOrigin,
+  credentials: true,
 }));
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Serve static files from the "public" directory
 app.use(express.static("public"));
 
-app.use("/api/v1",router)
+app.use("/api/v1", router);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server is running on port ${port} in ${process.env.NODE_ENV || 'development'} mode`);
+});
