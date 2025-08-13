20ca70e96a28ec8fdf02d2114eb6923e1cbd22fa
HUYDO <huydo041203@gmail.com>
2025-08-13 14:05:26 +0700

    update

--- src/db/migrations/20250812052913-create-comment-table.js
+++ src/db/migrations/20250812052913-create-comment-table.js
@@ -13,7 +13,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Posts',
+          model: 'posts',
           key: 'id',
         },
         onDelete: 'CASCADE',
@@ -22,7 +22,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Users',
+          model: 'users',
           key: 'id',
         },
         onDelete: 'CASCADE',
