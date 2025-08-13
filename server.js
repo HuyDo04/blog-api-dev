0e5614c02e8634fc186b3969ef9e8baa12024c2e
HUYDO <huydo041203@gmail.com>
2025-08-13 17:05:34 +0700

    update

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 require('dotenv').config();
 require('module-alias/register');
 
-console.log('PORT:', process.env.PORT);
-console.log('CLIENT_ORIGIN:', process.env.CLIENT_ORIGIN);
-console.log('NODE_ENV:', process.env.NODE_ENV);
 const cors = require("cors");
 const express = require('express');
 const router = require("./src/routes");
@@ -12,7 +9,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 const port = process.env.PORT || 3000;
-const clientOrigin = process.env.CLIENT_ORIGIN || "http://localhost:5173";
+const clientOrigin = process.env.FRONTEND_URL || "http://localhost:5173";
 
 app.use(cors({
   origin: clientOrigin,
